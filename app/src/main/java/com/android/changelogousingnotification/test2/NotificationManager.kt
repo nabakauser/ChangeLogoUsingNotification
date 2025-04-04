d9192893ef182f6f9c90eae809c4a201e58537f4
nabakauser <nabakauser.j@novosoftsolutions.in>
2025-04-04 11:29:28 +0530

    handle logo changes on notification received

--- app/src/main/java/com/android/changelogousingnotification/test2/NotificationManager.kt
+++ app/src/main/java/com/android/changelogousingnotification/test2/NotificationManager.kt
@@ -7,10 +7,12 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.android.changelogousingnotification.R
+import com.android.changelogousingnotification.changeAppIcon
 
 class NotificationManagerHelper(private val context: Context) {
 
@@ -38,15 +40,6 @@ class NotificationManagerHelper(private val context: Context) {
     }
 
     fun showTestNotification(logo: String) {
-        val dismissIntent = Intent(context, NotificationDismissReceiver::class.java).apply {
-            action = ACTION_DISMISS
-            putExtra("notificationId", NOTIFICATION_ID)
-        }
-        val dismissPendingIntent: PendingIntent =
-            PendingIntent.getBroadcast(
-                context, 0, dismissIntent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-            )
-
         val toastIntent = Intent(context, ToastReceiver::class.java).apply {
             putExtra("logoData",logo)
         }
@@ -54,14 +47,17 @@ class NotificationManagerHelper(private val context: Context) {
             context, 1, toastIntent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
+        handleNotificationData(
+            context, logo
+        )
+
         val builder = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle("APP TITLE")
-            .setContentText("Hello, this is test notification")
+            .setContentTitle("APP TITLE - $logo")
+            .setContentText("Hello, this is test notification for $logo")
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setStyle(NotificationCompat.BigTextStyle().bigText("Hello, this is a test notification"))
             .setAutoCancel(true)
-            .addAction(0, "Okay", dismissPendingIntent)
             .addAction(0, "Toast", toastPendingIntent)
 
         with(NotificationManagerCompat.from(context)) {
@@ -71,6 +67,34 @@ class NotificationManagerHelper(private val context: Context) {
             }
             notify(NOTIFICATION_ID, builder.build())
         }
+    }
 
+    private fun handleNotificationData(
+        context: Context?,
+        data: String
+    ) {
+        val alias = when (data) {
+            "colored" ->{
+                Log.d("AppLog", "colored: $data")
+                "${context?.packageName}.LogoOneActivity"
+            }
+            "blue" -> {
+                Log.d("AppLog", "blue: $data")
+                "${context?.packageName}.LogoTwoActivity"
+            }
+            else -> {
+                Log.d("AppLog", "else: $data")
+                "${context?.packageName}.MainActivity"
+            }
+        }
+
+        context?.changeAppIcon(
+            alias,
+            listOf(
+                "${context.packageName}.MainActivity",
+                "${context.packageName}.LogoOneActivity",
+                "${context.packageName}.LogoTwoActivity",
+            )
+        )
     }
 }
